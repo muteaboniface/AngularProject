2dc06bf2662d260b96476cff464d6d252788daf5
Boniface <muteaboniface@gmail.com>
2022-10-21 08:42:24 +0300

    additional components , directives, data binding

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class = "notif-bar"> 
+  template: `<div class = "notif-bar" [ngClass] =    "{fadeout: hideNotification}"> 
+              <div class="close"><button class="close" (click)="hideNotif()">X</button></div>
               <p> This website uses cookies to provide a great user experience </p>
               </div>`,
-  styles: [".notif-bar{margin: 0px 10px;padding: 10px 20px;text-align:center;background-color:yellow;}","p{font-size:45px;}"]
+  styles: [".notif-bar{margin: 0px 10px;padding: 10px 20px;text-align:center;background-color:magenta;}","p{font-size:25px;}",".close {float: right;margin-top: 0px;}",
+  ".fadeout {visibility: hidden;opacity: 0;transition: visibility 0s 2s, opacity 2s linear;}"]
 })
 export class NotificationComponent implements OnInit {
 
@@ -13,5 +15,9 @@ export class NotificationComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  hideNotification: boolean = false;
 
+  hideNotif(){
+    this.hideNotification = true;
+  }
 }
